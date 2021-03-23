c0fd666cfed1da772093888afcba162d8338341b
AnnaLasmane <anna.gravere@gmail.com>
2021-03-23 20:57:29 +0200

    taks

--- js/task10.js
+++ js/task10.js
@@ -1,9 +1,13 @@
+// function matrix(n) {
+//     return Array(n).fill(Array(n).fill())
+//     .map(function (xs, i) {
+//         return xs.map(function (x, j) {
+//             return i === j ? 1 : 0;
+//         })
+//     });
+// }
+// console.log(matrix(5));
+
 function matrix(n) {
-    return Array(n).fill(Array(n).fill())
-    .map(function (xs, i) {
-        return xs.map(function (x, j) {
-            return i === j ? 1 : 0;
-        })
-    });
+    return 
 }
-console.log(matrix(5));
